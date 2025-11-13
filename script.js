27851ad2a9c5a2150ac6453d951b4d4074b3b7e8
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2025-11-13 18:36:55 +0000

    terminando tudo.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+function acender() {
+    document.getElementById("lampada").src = "lampada.png";
+    document.getElementById("imagemExtra").src = "deboa.jpg";
+}
 
-function acender(){
-    document.getElementById("lampada").src="lampada.png"
-    console.log("ACENDER")
+function apagar() {
+    document.getElementById("lampada").src = "lampadaapagada.png";
+    document.getElementById("imagemExtra").src = "nodeboa.jpg";
+}
+
+function eclipse() {
+    document.getElementById("lampada").src = "eclipse.png";
+    document.getElementById("imagemExtra").src = "assustado.jpg";
+}
+
+function churrasco() {
+    document.getElementById("lampada").src = "churrasco.png";
+    document.getElementById("imagemExtra").src = "feliz.jpg";
 }
